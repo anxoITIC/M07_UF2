497acc9c82922461268ddb696be1321fb3cd19fd
Alex Andreev <2023_alexander.andreev@iticbcn.cat>
2023-12-14 17:24:43 +0100

    Afegides rutes per a la gestió completa de l'alumnat (index, create, store, show, edit, update, destroy) al controlador AlumnatController.

--- practica4/routes/admin.php
+++ practica4/routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\AlumnatController;
 
 // Agrupar les rutes amb el middleware 'admin_db'
 Route::middleware(['admin_db'])->group(function () {
@@ -15,7 +16,28 @@ Route::middleware(['admin_db'])->group(function () {
     Route::get('/centres', [AdminController::class, 'centres'])->name('centres');
 
     // Ruta GET per mostrar la vista de gestió de l'alumnat
-    Route::get('/alumnat', [AdminController::class, 'alumnat'])->name('alumnat');
+    //Route::get('/alumnat', [AdminController::class, 'alumnat'])->name('alumnat');
+
+    // Ruta per mostrar la llista d'alumnes
+    Route::get('/alumnat', [AlumnatController::class, 'index'])->name('alumnat.index');
+
+    // Ruta per mostrar el formulari de creació d'un nou alumne
+    Route::get('/alumnat/create', [AlumnatController::class, 'create'])->name('alumnat.create');
+
+    // Ruta per emmagatzemar un nou alumne creat a la base de dades
+    Route::post('/alumnat', [AlumnatController::class, 'store'])->name('alumnat.store');
+
+    // Ruta per mostrar els detalls d'un alumne específic
+    Route::get('/alumnat/{alumne}', [AlumnatController::class, 'show'])->name('alumnat.show');
+
+    // Ruta per mostrar el formulari d'edició d'un alumne específic
+    Route::get('/alumnat/{alumne}/edit', [AlumnatController::class, 'edit'])->name('alumnat.edit');
+
+    // Ruta per actualitzar les dades d'un alumne específic a la base de dades
+    Route::put('/alumnat/{alumne}', [AlumnatController::class, 'update'])->name('alumnat.update');
+
+    // Ruta per eliminar un alumne específic de la base de dades
+    Route::delete('/alumnat/{alumne}', [AlumnatController::class, 'destroy'])->name('alumnat.destroy');
 
     // Ruta GET per mostrar la vista de gestió del professorat
     Route::get('/professorat', [AdminController::class, 'professorat'])->name('professorat');
